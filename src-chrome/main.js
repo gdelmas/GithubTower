9e7c58a39b9a2648002ae32b0988ad95239b9c35
Gerard Delmàs <gdelmas@dreamlab.de>
2018-01-18 02:11:17 +0100

    support not logged in users & toggling HTTPS/SSH

--- src-chrome/main.js
+++ src-chrome/main.js
@@ -1,7 +1,23 @@
 (function() {
-    var buttonElement = document.querySelector("a[data-url^='github-mac://']");
+    var buttonElement = document.querySelector("a.get-repo-btn");
 
-    if ( buttonElement != null ) {
-        buttonElement.href = buttonElement.getAttribute("data-url");
+    if ( buttonElement === null ) {
+        return;
     }
+
+    buttonElement.removeAttribute('href');
+    buttonElement.innerText = 'Open in Tower';
+    buttonElement.setAttribute('aria-label', buttonElement.getAttribute('aria-label').replace('GitHub Desktop', 'Tower'));
+
+    buttonElement.addEventListener('click', function(event) {
+        event.preventDefault();
+
+        var repoModalElement = buttonElement.parentElement.parentElement;
+        var inputElements = repoModalElement.querySelectorAll('input[type="text"]');
+        var isSSH = repoModalElement.classList.contains('on');
+
+        var activeInputElement = inputElements[isSSH ? 1 : 0];
+
+        window.location.href = 'gittower://openRepo/' + encodeURI(activeInputElement.value);
+    });
 }());
